e933fa3bae4a6beb41182262d6725d3b2bbacbc5
AmirHosein Mousavi <ineptant@gmail.com>
2024-09-29 15:31:19 +0200

    Update api urls, add tx inquiry and reverse tx, etc

--- README.md
+++ README.md
@@ -1,35 +1,3 @@
 # Zarinpal Checkout
 
-[![GoDoc](https://godoc.org/github.com/sinabakh/go-zarinpal-checkout?status.svg)](https://godoc.org/github.com/sinabakh/go-zarinpal-checkout)
-[![Go Report Card](https://goreportcard.com/badge/github.com/sinabakh/go-zarinpal-checkout)](https://goreportcard.com/report/github.com/sinabakh/go-zarinpal-checkout)
-
-Unofficial [Zarinpal Checkout](https://www.zarinpal.com/) implementation in Golang.
-
-You can read the docs [HERE](https://godoc.org/github.com/sinabakh/go-zarinpal-checkout).
-
-## Installation
-
-Assuming you already have a recent version of Go installed, pull down the code with go get:
-
-```bash
-go get github.com/sinabakh/go-zarinpal-checkout
-```
-
-## Contributing
-
-Contributions are welcome. Please submit PRs or just file an Issue if you see something broken or in need of improving.
-
-## License
-
-    Copyright © 2017 Sina Bakhtiari <sinabakh44@gmail.com>
-    This work is free. You can redistribute it and/or modify it under the
-    terms of the Beerware License, as published by Poul-Henning Kamp. See below for more details.
-
-    /*
-    * ----------------------------------------------------------------------------
-    * "THE BEER-WARE LICENSE" (Revision 42):
-    * <phk@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
-    * can do whatever you want with this stuff. If we meet some day, and you think
-    * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
-    *  ----------------------------------------------------------------------------
-    */
+[Zarinpal](https://www.zarinpal.com/) payment gateway implementation with Golang.
--- main.go
+++ main.go
@@ -1,5 +1,3 @@
-// Package zarinpal provides simple methods to work
-// with Zarinpal (https://www.zarinpal.com/) checkout gateway
 package zarinpal
 
 import (
@@ -7,15 +5,10 @@ import (
 	"encoding/json"
 	"errors"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-// Zarinpal is the base struct for zarinpal payment
-// gateway, one shall not create or manipulate instances
-// if this struct manually and just use provided methods
-// to woek with it.
 type Zarinpal struct {
 	MerchantID      string
 	Sandbox         bool
@@ -24,93 +17,116 @@ type Zarinpal struct {
 }
 
 type paymentRequestReqBody struct {
-	MerchantID  string
-	Amount      int
-	CallbackURL string
-	Description string
-	Email       string
-	Mobile      string
+	MerchantID  string `json:"merchant_id"`
+	Amount      int    `json:"amount"`
+	Description string `json:"description"`
+	CallbackURL string `json:"callback_url"`
+}
+
+type paymentRequestRespData struct {
+	Authority string `json:"authority"`
+	Fee       int    `json:"fee"`
+	FeeType   string `json:"fee_type"`
+	Status    int    `json:"code"`
+	Message   string `json:"message"`
 }
 
 type paymentRequestResp struct {
-	Status    int
-	Authority string
+	Data   paymentRequestRespData
+	Errors []any
 }
 
 type paymentVerificationReqBody struct {
-	MerchantID string
-	Authority  string
-	Amount     int
+	MerchantID string `json:"merchant_id"`
+	Amount     int    `json:"amount"`
+	Authority  string `json:"authority"`
+}
+
+type paymentVerificationRespData struct {
+	Status    int         `json:"code"`
+	Message   string      `json:"message"`
+	CardHash  string      `json:"card_hash"`
+	CardPan   string      `json:"card_pan"`
+	RefID     json.Number `json:"ref_id"`
+	FeeType   string      `json:"fee_type"`
+	Fee       int         `json:"fee"`
 }
 
 type paymentVerificationResp struct {
-	Status int
-	RefID  json.Number
+	Data   paymentVerificationRespData
+	Errors []any
 }
 
 type unverifiedTransactionsReqBody struct {
-	MerchantID string
+	MerchantID string `json:"merchant_id"`
 }
 
-// UnverifiedAuthority is the base struct for Authorities in unverifiedTransactionsResp
 type UnverifiedAuthority struct {
-	Authority   string
-	Amount      int
-	Channel     string
-	CallbackURL string
-	Referer     string
-	Email       string
-	CellPhone   string
-	Date        string // ToDo Check type to be date
+	Authority   string `json:"authority"`
+	Amount      int    `json:"amount"`
+	CallbackURL string `json:"callback_url"`
+	Referer     string `json:"referer"`
+	Date        string `json:"date"`
+}
+
+type unverifiedTransactionsRespData struct {
+	Status      int                   `json:"code"`
+	Message     string                `json:"message"`
+	Authorities []UnverifiedAuthority `json:"authorities"`
 }
 
 type unverifiedTransactionsResp struct {
-	Status      int
-	Authorities []UnverifiedAuthority
+	Data unverifiedTransactionsRespData
 }
 
-type refreshAuthorityReqBody struct {
-	MerchantID string
-	Authority  string
-	ExpireIn   int
+type transactionInquiryReqBody struct {
+	MerchantID string `json:"merchant_id"`
+	Authority  string `json:"authority"`
 }
 
-type refreshAuthorityResp struct {
-	Status int
+type transactionInquiryRespData struct {
+	Code    string `json:"status"`
+	Status  int    `json:"code"`
+	Message string `json:"message"`
 }
 
-// NewZarinpal creates a new instance of zarinpal payment
-// gateway with provided configs. It also tries to validate
-// provided configs.
-func NewZarinpal(merchantID string, sandbox bool) (*Zarinpal, error) {
+type transactionInquiryResp struct {
+	Data    transactionInquiryRespData
+	Message string
+	Errors  []any
+}
+
+type reverseTransactionRespData struct {
+	Status  int    `json:"code"`
+	Message string `json:"message"`
+}
+
+type reverseTransactionResp struct {
+	Data    reverseTransactionRespData
+	Errors  []any
+}
+
+func New(merchantID string, sandbox bool) (*Zarinpal, error) {
 	if len(merchantID) != 36 {
 		return nil, errors.New("MerchantID must be 36 characters")
 	}
-	apiEndPoint := "https://www.zarinpal.com/pg/rest/WebGate/"
-	paymentEndpoint := "https://www.zarinpal.com/pg/StartPay/"
+
+	apiEndpoint := "https://api.zarinpal.com/pg/v4/payment/"
+	paymentEndpoint := "https://payment.zarinpal.com/pg/StartPay/"
 	if sandbox == true {
-		apiEndPoint = "https://sandbox.zarinpal.com/pg/rest/WebGate/"
+		apiEndpoint = "https://sandbox.zarinpal.com/pg/v4/payment/"
 		paymentEndpoint = "https://sandbox.zarinpal.com/pg/StartPay/"
 	}
+
 	return &Zarinpal{
 		MerchantID:      merchantID,
 		Sandbox:         sandbox,
-		APIEndpoint:     apiEndPoint,
+		APIEndpoint:     apiEndpoint,
 		PaymentEndpoint: paymentEndpoint,
 	}, nil
 }
 
-// NewPaymentRequest gets a payment url from Zarinpal.
-// amount is in Tomans (not Rials) format.
-// email and mobile are optional.
-//
-// If error is not nil, you can check statusCode for
-// specific error handling based on Zarinpal error codes.
-// If statusCode is not 100, it means Zarinpal raised an error
-// on their end and you can check the error code and its reason
-// based on their documentation placed in
-// https://github.com/ZarinPal-Lab/Documentation-PaymentGateway/archive/master.zip
-func (zarinpal *Zarinpal) NewPaymentRequest(amount int, callbackURL, description, email, mobile string) (paymentURL, authority string, statusCode int, err error) {
+func (zarinpal *Zarinpal) NewPayment(amount int, callbackURL, description string) (paymentURL, authority string, statusCode int, err error) {
 	if amount < 1 {
 		err = errors.New("amount must be a positive number")
 		return
@@ -123,39 +139,31 @@ func (zarinpal *Zarinpal) NewPaymentRequest(amount int, callbackURL, description
 		err = errors.New("description should not be empty")
 		return
 	}
+
 	paymentRequest := paymentRequestReqBody{
 		MerchantID:  zarinpal.MerchantID,
 		Amount:      amount,
-		CallbackURL: callbackURL,
 		Description: description,
-		Email:       email,
-		Mobile:      mobile,
+		CallbackURL: callbackURL,
 	}
 	var resp paymentRequestResp
-	err = zarinpal.request("PaymentRequest.json", &paymentRequest, &resp)
+	err = zarinpal.request(zarinpal.APIEndpoint+"request.json", &paymentRequest, &resp)
 	if err != nil {
 		return
 	}
-	statusCode = resp.Status
-	if resp.Status == 100 {
-		authority = resp.Authority
-		paymentURL = zarinpal.PaymentEndpoint + resp.Authority
+
+	statusCode = resp.Data.Status
+	if resp.Data.Status == 100 {
+		authority = resp.Data.Authority
+		paymentURL = zarinpal.PaymentEndpoint + resp.Data.Authority
 	} else {
-		err = errors.New(strconv.Itoa(resp.Status))
+		err = errors.New(strconv.Itoa(resp.Data.Status))
 	}
+
 	return
 }
 
-// PaymentVerification verifies if a payment was done successfully, Authority of the
-// payment request should be passed to this method alongside its Amount in Tomans.
-//
-// If error is not nil, you can check statusCode for
-// specific error handling based on Zarinpal error codes.
-// If statusCode is not 100, it means Zarinpal raised an error
-// on their end and you can check the error code and its reason
-// based on their documentation placed in
-// https://github.com/ZarinPal-Lab/Documentation-PaymentGateway/archive/master.zip
-func (zarinpal *Zarinpal) PaymentVerification(amount int, authority string) (verified bool, refID string, statusCode int, err error) {
+func (zarinpal *Zarinpal) VerifyTransaction(amount int, authority string) (verified bool, refID string, statusCode int, err error) {
 	if amount <= 0 {
 		err = errors.New("amount must be a positive number")
 		return
@@ -164,119 +172,129 @@ func (zarinpal *Zarinpal) PaymentVerification(amount int, authority string) (ver
 		err = errors.New("authority should not be empty")
 		return
 	}
+
 	paymentVerification := paymentVerificationReqBody{
 		MerchantID: zarinpal.MerchantID,
 		Amount:     amount,
 		Authority:  authority,
 	}
 	var resp paymentVerificationResp
-	err = zarinpal.request("PaymentVerification.json", &paymentVerification, &resp)
+	err = zarinpal.request(zarinpal.APIEndpoint+"verify.json", &paymentVerification, &resp)
 	if err != nil {
 		return
 	}
-	statusCode = resp.Status
-	if resp.Status == 100 {
+
+	statusCode = resp.Data.Status
+	if resp.Data.Status == 100 {
 		verified = true
-		refID = string(resp.RefID)
+		refID = string(resp.Data.RefID)
 	} else {
-		err = errors.New(strconv.Itoa(resp.Status))
+		err = errors.New(strconv.Itoa(resp.Data.Status))
 	}
+
 	return
 }
 
-// UnverifiedTransactions gets unverified transactions.
-//
-// If error is not nil, you can check statusCode for
-// specific error handling based on Zarinpal error codes.
-// If statusCode is not 100, it means Zarinpal raised an error
-// on their end and you can check the error code and its reason
-// based on their documentation placed in
-// https://github.com/ZarinPal-Lab/Documentation-PaymentGateway/archive/master.zip
 func (zarinpal *Zarinpal) UnverifiedTransactions() (authorities []UnverifiedAuthority, statusCode int, err error) {
 	unverifiedTransactions := unverifiedTransactionsReqBody{
 		MerchantID: zarinpal.MerchantID,
 	}
-
 	var resp unverifiedTransactionsResp
-	err = zarinpal.request("UnverifiedTransactions.json", &unverifiedTransactions, &resp)
+	err = zarinpal.request(zarinpal.APIEndpoint+"unVerified.json", &unverifiedTransactions, &resp)
 	if err != nil {
 		return
 	}
 
-	if resp.Status == 100 {
-		statusCode = resp.Status
-		authorities = resp.Authorities
+	if resp.Data.Status == 100 {
+		statusCode = resp.Data.Status
+		authorities = resp.Data.Authorities
 	} else {
-		err = errors.New(strconv.Itoa(resp.Status))
+		err = errors.New(strconv.Itoa(resp.Data.Status))
 	}
+
 	return
 }
 
-// RefreshAuthority update authority expiration time.\n
-// expire should be number between [1800,3888000] seconds.
-//
-// If error is not nil, you can check statusCode for
-// specific error handling based on Zarinpal error codes.
-// If statusCode is not 100, it means Zarinpal raised an error
-// on their end and you can check the error code and its reason
-// based on their documentation placed in
-// https://github.com/ZarinPal-Lab/Documentation-PaymentGateway/archive/master.zip
-func (zarinpal *Zarinpal) RefreshAuthority(authority string, expire int) (statusCode int, err error) {
+func (zarinpal *Zarinpal) TransactionInquiry(authority string) (inquiry string, statusCode int, err error) {
 	if authority == "" {
 		err = errors.New("authority should not be empty")
 		return
 	}
-	if expire < 1800 {
-		err = errors.New("expire must be at least 1800")
-		return
-	} else if expire > 3888000 {
-		err = errors.New("expire must not be greater than 3888000")
-		return
-	}
 
-	refreshAuthority := refreshAuthorityReqBody{
+	transactionInquiry := transactionInquiryReqBody{
 		MerchantID: zarinpal.MerchantID,
 		Authority:  authority,
-		ExpireIn:   expire,
 	}
-	var resp refreshAuthorityResp
-	err = zarinpal.request("RefreshAuthority.json", &refreshAuthority, &resp)
+	var resp transactionInquiryResp
+	err = zarinpal.request(zarinpal.APIEndpoint+"inquiry.json", &transactionInquiry, &resp)
 	if err != nil {
 		return
 	}
-	if resp.Status == 100 {
-		statusCode = resp.Status
+
+	if resp.Data.Status == 100 {
+		statusCode = resp.Data.Status
+		inquiry = resp.Data.Code
 	} else {
-		err = errors.New(strconv.Itoa(resp.Status))
+		err = errors.New(strconv.Itoa(resp.Data.Status))
 	}
+
 	return
 }
 
-func (zarinpal *Zarinpal) request(method string, data interface{}, res interface{}) error {
+func (zarinpal *Zarinpal) ReverseTransaction(authority string) (statusCode int, err error) {
+	if authority == "" {
+		err = errors.New("authority should not be empty")
+		return
+	}
+
+	transactionInquiry := transactionInquiryReqBody{
+		MerchantID: zarinpal.MerchantID,
+		Authority:  authority,
+	}
+	var resp reverseTransactionResp
+	err = zarinpal.request(zarinpal.APIEndpoint+"reverse.json", &transactionInquiry, &resp)
+	if err != nil {
+		return
+	}
+
+	if resp.Data.Status == 100 {
+		statusCode = resp.Data.Status
+	} else {
+		err = errors.New(strconv.Itoa(resp.Data.Status))
+	}
+
+	return
+}
+
+func (zarinpal *Zarinpal) request(endpoint string, data interface{}, res interface{}) error {
 	reqBytes, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
-	req, err := http.NewRequest("POST", zarinpal.APIEndpoint+method, bytes.NewBuffer(reqBytes))
+
+	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(reqBytes))
 	if err != nil {
 		return err
 	}
+
 	req.Header.Set("Content-Type", "application/json")
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		return err
 	}
+
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
-	log.Println(string(body))
+
 	err = json.Unmarshal(body, res)
 	if err != nil {
 		err = errors.New("zarinpal invalid json response")
 		return err
 	}
+	
 	return nil
 }
